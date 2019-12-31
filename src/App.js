c207b14090c191d1808999f23e95923f89c8dd44
Alberto Barreda <aabarreda@gmail.com>
2019-12-31 15:18:24 -0800

    added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Stack } from 'office-ui-fabric-react/lib/Stack';
 
 function App() {
   return (
@@ -20,6 +21,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <Stack>
+        <span>Item One</span>
+        <span>Item Two</span>
+        <span>Item Thredde</span>
+      </Stack>
     </div>
   );
 }
