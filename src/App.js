12fa7781583606bfe6217f1ba1ed3ab38693be86
Alberto Barreda <aabarreda@gmail.com>
2020-01-02 23:55:33 -0800

    updated a lot

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/logo.jpg';
 import './App.css';
 import { Stack } from 'office-ui-fabric-react/lib/Stack';
+import { Image } from 'office-ui-fabric-react/lib/Image';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          text
-      </header>
-      <Stack horizontal wrap>
+    <header className="App-header">
+        <Image className='logo' imageFit={1} src={logo}/>
+        610 N. Cunningham Urbana, IL. 61802 <br/>
+        Phone: 217-344-2020  <br/>
+        Fax: 217-344-2022
+    </header>
+      <Stack horizontal horizontalAlign='center' wrap>
+        <div>text</div>
+        <div>text</div>
+        <div>text</div>
+        <div>text</div>
+        <div>text</div>
+        <div>text</div>
+        <div>text</div>
         <span>Item One</span>
         <span>Item Two</span>
         <span>Item Three</span>
@@ -29,6 +40,17 @@ function App() {
         <span>Item Two</span>
         <span>Item Three</span>
       </Stack>
+      <Stack horizontal horizontalAlign='center' wrap>
+          <Stack>
+            Our Location
+          </Stack>
+          <Stack>
+            Contact Us
+          </Stack>
+      </Stack>
+      <footer className="App-header">
+        Copyright @ 2020 BBA Auto Sales
+      </footer>
     </div>
   );
 }
