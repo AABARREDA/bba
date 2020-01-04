7ce722eda7208119470298c8058c7cec3cfb46db
Alberto Barreda <aabarreda@gmail.com>
2020-01-04 00:38:55 -0800

    created tiles

--- src/App.js
+++ src/App.js
@@ -18,38 +18,33 @@ function App() {
     </header>
     <Separator className='App-header'>Current Inventory</Separator>
       <Stack horizontal horizontalAlign='center' wrap>
-        <div>text</div>
-        <div>text</div>
-        <div>text</div>
-        <div>text</div>
-        <div>text</div>
-        <div>text</div>
-        <div>text</div>
-        <span>Item One</span>
-        <span>Item Two</span>
-        <span>Item Three</span>
-        <span>Item One</span>
-        <span>Item Two</span>
-        <span>Item Three</span>
-        <span>Item One</span>
-        <span>Item Two</span>
-        <span>Item Three</span>
-        <span>Item One</span>
-        <span>Item Two</span>
-        <span>Item Three</span>
-        <span>Item One</span>
-        <span>Item Two</span>
-        <span>Item Three</span>
-        <span>Item One</span>
-        <span>Item Two</span>
-        <span>Item Three</span>
+          <Stack horizontalAlign='center' className='tile'>
+            <Image className='tile-pic' imageFit={1} src={logo}/>
+            Car 1
+          </Stack>
+          <Stack horizontalAlign='center' className='tile'>
+            <Image className='tile-pic' imageFit={1} src={logo}/>
+            Car 1
+          </Stack>
+          <Stack horizontalAlign='center' className='tile'>
+            <Image className='tile-pic' imageFit={1} src={logo}/>
+            Car 1
+          </Stack>
+          <Stack horizontalAlign='center' className='tile'>
+            <Image className='tile-pic' imageFit={1} src={logo}/>
+            Car 1
+          </Stack>
+          <Stack horizontalAlign='center' className='tile'>
+            <Image className='tile-pic' imageFit={1} src={logo}/>
+            Car 1
+          </Stack>
       </Stack>
       <Separator className='App-header'>Our Location</Separator>
       <Stack horizontal horizontalAlign='center' wrap>
           <Stack>
           <Iframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3051.0649338166468!2d-88.20565628435764!3d40.11855638164319!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880cd79c8a9ba8bb%3A0xbe55a6bc792bdab1!2sBBA%20Auto%20Sales%20LLC!5e0!3m2!1sen!2sus!4v1578122642967!5m2!1sen!2sus"
-            width="600px"
-            height="450px"
+            width="500px"
+            height="350px"
             id="myId"
             className="myClassname"
             display="initial"
