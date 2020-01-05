55d4d05c20f55ed1699cb97f3c72f1b7ef313dd0
Alberto Barreda <aabarreda@gmail.com>
2020-01-05 01:09:54 -0800

    updated info and added one picture

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './assets/logo.jpg';
+import tahoe from './assets/trucks/ChevyTahoe2008.jpg';
 import './App.css';
 import { Stack } from 'office-ui-fabric-react/lib/Stack';
 import { Image } from 'office-ui-fabric-react/lib/Image';
@@ -23,7 +24,7 @@ function App() {
 
       <Stack horizontal horizontalAlign='center' wrap>
           <Stack horizontalAlign='center' className='tile'>
-            <Image className='tile-pic' imageFit={1} src={logo}/>
+            <Image className='tile-pic' imageFit={1} src={tahoe}/>
             <Text> 2008 Chevy Tahoe $10500, 121K Miles, All Power, Third Row Seat, Cold AC, Alloy Wheels, Good Tires, Flex Fuel. 4 pics.</Text>
           </Stack>
           <Stack horizontalAlign='center' className='tile'>
@@ -130,7 +131,7 @@ function App() {
 
       <Separator className='App-header'>Info</Separator>
 
-      <Stack horizontal horizontalAlign='center' wrap>
+      <Stack horizontal horizontalAlign='center' wrap gap='15'>
           <Stack>
           <Text className='App-info'>Our Location </Text>
           <Iframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3051.0649338166468!2d-88.20565628435764!3d40.11855638164319!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880cd79c8a9ba8bb%3A0xbe55a6bc792bdab1!2sBBA%20Auto%20Sales%20LLC!5e0!3m2!1sen!2sus!4v1578122642967!5m2!1sen!2sus"
@@ -142,7 +143,10 @@ function App() {
             position="relative"/>
           </Stack>
           <Stack className='tile-about'>
-          <Text className='App-info'>About Us </Text>
+            <Text className='App-info'>About Us</Text>
+            <Text className='App-info'>BBA Auto Sales is family owned and operated. We have been part of the Urbana community for the last 27 years. 
+            Being a small family owned dealership allows us to keep our prices low.</Text><br/>
+            <Text horizontalAlign='right' className='App-info'>Antonio Barreda - Owner</Text>
           </Stack>
       </Stack>
       <footer className="App-footer">
