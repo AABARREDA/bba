09be1ef22903f8d73c2ce36ccba545eb0bd493fe
Alberto Barreda <aabarreda@gmail.com>
2020-01-05 19:35:39 -0800

    updated webpage

--- src/App.js
+++ src/App.js
@@ -187,7 +187,7 @@ function App() {
 
       <Stack horizontal horizontalAlign='center' wrap gap='50'>
           <Stack>
-          <Text className='App-info'>Our Location </Text>
+          <Text className='App-info-test'>Our Location </Text>
           <Iframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3051.0649338166468!2d-88.20565628435764!3d40.11855638164319!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880cd79c8a9ba8bb%3A0xbe55a6bc792bdab1!2sBBA%20Auto%20Sales%20LLC!5e0!3m2!1sen!2sus!4v1578122642967!5m2!1sen!2sus"
             width="500px"
             height="350px"
@@ -197,10 +197,10 @@ function App() {
             position="relative"/>
           </Stack>
           <Stack className='tile-about'>
-            <Text className='App-info'>About Us</Text>
-            <Text className='App-info'>BBA Auto Sales is family owned and operated. We have been part of the Urbana community for the last 27 years. 
+            <Text className='App-info-test'>About Us</Text>
+            <Text className='App-info-test'>BBA Auto Sales is family owned and operated. We have been part of the Urbana community for the last 27 years. 
             Being a small family owned dealership allows us to provide quality cars while keeping our prices low.</Text><br/>
-            <Text horizontalAlign='right' className='App-info'>Antonio Barreda - Owner</Text>
+            <Text horizontalAlign='right' className='App-info-test'>Antonio Barreda - Owner</Text>
           </Stack>
       </Stack>
       <footer className="App-footer">
