34ec39a46dd2de8e17add6dcb4ed4fd4d6b3ec68
Alberto Barreda <aabarreda@gmail.com>
2020-01-21 00:03:12 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2a24abe7fa563a54dda83724a775489f.js"
+  "/precache-manifest.3c605790caaaca65f7c39794e55eac8d.js"
 );
 
 self.addEventListener('message', (event) => {
