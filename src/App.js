f56180261e09b1b01db34b6312c4f84721a4a1b0
Alberto Barreda <aabarreda@gmail.com>
2020-01-21 19:28:59 -0800

    updated map

--- src/App.js
+++ src/App.js
@@ -35,11 +35,11 @@ function App() {
 
       <Separator className='App-header'>Info</Separator>
 
-      <Stack horizontal horizontalAlign='center' wrap gap='50'>
+      <Stack horizontal horizontalAlign='center' wrap gap='15'>
           <Stack className='tile-about'>
           <Text className='App-info-test'>Our Location </Text>
           <Iframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3051.0649338166468!2d-88.20565628435764!3d40.11855638164319!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880cd79c8a9ba8bb%3A0xbe55a6bc792bdab1!2sBBA%20Auto%20Sales%20LLC!5e0!3m2!1sen!2sus!4v1578122642967!5m2!1sen!2sus"
-            width="450px"
+            width="425px"
             height="350px"
             display="initial"
             />
