ace4e60eab044f8cab660598d825f280af5dab98
Alberto Barreda <aabarreda@gmail.com>
2020-01-21 19:19:20 -0800

    Updates

---
+++ precache-manifest.797cb4e336165308fda148d09175a3a8.js
@@ -0,0 +1,342 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ca7a7454acdacd64a5fb4c21726158",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c41d0f079f9dfd91dfe",
+    "url": "/static/css/2.65eee298.chunk.css"
+  },
+  {
+    "revision": "a15af15e3e59b14bf985",
+    "url": "/static/css/main.9373a091.chunk.css"
+  },
+  {
+    "revision": "8c41d0f079f9dfd91dfe",
+    "url": "/static/js/2.77a45b79.chunk.js"
+  },
+  {
+    "revision": "7ce361e72a1a624a914acb306ecb376d",
+    "url": "/static/js/2.77a45b79.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a15af15e3e59b14bf985",
+    "url": "/static/js/main.975771f1.chunk.js"
+  },
+  {
+    "revision": "dfda05006be39051ceae",
+    "url": "/static/js/runtime-main.1968646e.js"
+  },
+  {
+    "revision": "74d00b98f1f2d6ad8d19489a2fe49b3b",
+    "url": "/static/media/BuickCentury2004.74d00b98.JPG"
+  },
+  {
+    "revision": "08084596950171d0bc41da63d170eae0",
+    "url": "/static/media/ChevyHHR2011.08084596.JPG"
+  },
+  {
+    "revision": "e2d7b676ebded306ed63c1c72e32381a",
+    "url": "/static/media/ChevyHHR20112.e2d7b676.JPG"
+  },
+  {
+    "revision": "5a6966f34042096b2cdb0dcd64d1f3fd",
+    "url": "/static/media/ChevyTahoe16.5a6966f3.JPG"
+  },
+  {
+    "revision": "d16c4ad22ea48f74e3add7a91748cdb6",
+    "url": "/static/media/ChevyTahoe2008.d16c4ad2.jpg"
+  },
+  {
+    "revision": "9bb023197d8f0dbc9c2cd46e06172ecd",
+    "url": "/static/media/FordFocus.9bb02319.JPG"
+  },
+  {
+    "revision": "31d72df4a257344d23813d34d6953c80",
+    "url": "/static/media/FordMustang.31d72df4.JPG"
+  },
+  {
+    "revision": "ca7885ae68076b065490b1333c139266",
+    "url": "/static/media/GTI.ca7885ae.JPG"
+  },
+  {
+    "revision": "2bb4632052d02205c9bd9e923e3fde81",
+    "url": "/static/media/HondaCivic.2bb46320.JPG"
+  },
+  {
+    "revision": "38d71dfecc898f9b1a017ac4fd8e5430",
+    "url": "/static/media/KiaRio2012.38d71dfe.JPG"
+  },
+  {
+    "revision": "c504ad502c6dc8a018656c60e39af7c8",
+    "url": "/static/media/KiaSportage2007.c504ad50.JPG"
+  },
+  {
+    "revision": "b3a300ac466387f9689bab3db66e2e20",
+    "url": "/static/media/MKX.b3a300ac.JPG"
+  },
+  {
+    "revision": "7c65ef543a43e31d2e93646a3a92bb06",
+    "url": "/static/media/Nissan.7c65ef54.JPEG"
+  },
+  {
+    "revision": "970f67c3afdaa8b05f58900804a4b981",
+    "url": "/static/media/NissanVersa2007.970f67c3.JPG"
+  },
+  {
+    "revision": "fd2168532217e93a0467abe9c2a4d860",
+    "url": "/static/media/NissanVersa2008.fd216853.JPG"
+  },
+  {
+    "revision": "382f7a4867585e9192a973feb0792996",
+    "url": "/static/media/PontiacG6.382f7a48.JPG"
+  },
+  {
+    "revision": "ca1860c1d597639540503ebade16a357",
+    "url": "/static/media/Quest.ca1860c1.JPG"
+  },
+  {
+    "revision": "4d29e259dd9d47b32fb0f45fc8792a15",
+    "url": "/static/media/S4.4d29e259.JPG"
+  },
+  {
+    "revision": "6a6caf3eec1a228111a50f2e2454320e",
+    "url": "/static/media/SRX.6a6caf3e.jpeg"
+  },
+  {
+    "revision": "40d87800c744ef7a3a9535ddc7e786a1",
+    "url": "/static/media/Sienna.40d87800.JPG"
+  },
+  {
+    "revision": "57e3d00d87f5a82c9c3c4a2991d98e7f",
+    "url": "/static/media/Terrain.57e3d00d.JPG"
+  },
+  {
+    "revision": "d03e5896e33b4930269fcae031f25f92",
+    "url": "/static/media/TrailBlazer.d03e5896.JPG"
+  },
+  {
+    "revision": "6e4ad620c1062770cb131f370e4f476c",
+    "url": "/static/media/caliber.6e4ad620.JPG"
+  },
+  {
+    "revision": "69cfbd8428a647f37eacbccf393ca577",
+    "url": "/static/media/caliber2.69cfbd84.JPG"
+  },
+  {
+    "revision": "95a9aacff5430c689020f699e86a067f",
+    "url": "/static/media/cbr2.95a9aacf.JPG"
+  },
+  {
+    "revision": "035f4d3dac09aded3c0f3cff10f773a9",
+    "url": "/static/media/cbr3.035f4d3d.JPG"
+  },
+  {
+    "revision": "33c8ac664d8203c20df35dfc69da4460",
+    "url": "/static/media/cbr4.33c8ac66.JPG"
+  },
+  {
+    "revision": "4ab8855628af5f9faa7d56b7859189c1",
+    "url": "/static/media/century2.4ab88556.JPG"
+  },
+  {
+    "revision": "ab0702b7cb543b88a220fd05b201559e",
+    "url": "/static/media/century3.ab0702b7.JPG"
+  },
+  {
+    "revision": "f7462a9cdb8da8068dcff7e2fa026d40",
+    "url": "/static/media/civic2.f7462a9c.JPG"
+  },
+  {
+    "revision": "318c1288d2902c100f187aa46f216cab",
+    "url": "/static/media/civic3.318c1288.JPG"
+  },
+  {
+    "revision": "b7694f8402393e0471a5bdc6a08d3897",
+    "url": "/static/media/dealer2.b7694f84.jpg"
+  },
+  {
+    "revision": "036b6764c31fa81f142b61a094b74743",
+    "url": "/static/media/focus2.036b6764.JPG"
+  },
+  {
+    "revision": "c272e672fce320a40643955154721048",
+    "url": "/static/media/focus3.c272e672.JPG"
+  },
+  {
+    "revision": "52c951fa43b592353eb923c64e8fb957",
+    "url": "/static/media/g62.52c951fa.JPG"
+  },
+  {
+    "revision": "be3b41b126c8e9ad18a34c60f518f553",
+    "url": "/static/media/g63.be3b41b1.JPG"
+  },
+  {
+    "revision": "bb81c6780e31b781f75abdfdaaad70d3",
+    "url": "/static/media/gti2.bb81c678.JPG"
+  },
+  {
+    "revision": "8b38ed2be72dca4c2d02f0b3951b360c",
+    "url": "/static/media/gti3.8b38ed2b.JPG"
+  },
+  {
+    "revision": "c95dfa0441d711d41e5a24792238e774",
+    "url": "/static/media/gti4.c95dfa04.JPG"
+  },
+  {
+    "revision": "49926daaa9a616c1cb00104b3f58a0f6",
+    "url": "/static/media/hhr2.49926daa.JPG"
+  },
+  {
+    "revision": "03fdebcd67ce46a6b4cfa66570ae6cd1",
+    "url": "/static/media/hhr22.03fdebcd.JPG"
+  },
+  {
+    "revision": "92f3605fa4e8afaca1a976a4c7d21aeb",
+    "url": "/static/media/hhr33.92f3605f.JPG"
+  },
+  {
+    "revision": "e3e5a46d56b80d44d6452784034d4dc5",
+    "url": "/static/media/hondacbr.e3e5a46d.JPG"
+  },
+  {
+    "revision": "62a0bc3bcbca43e341ae3f9c798e987a",
+    "url": "/static/media/kia2.62a0bc3b.JPG"
+  },
+  {
+    "revision": "4191043a7ad1f4250397f94d96d1f92b",
+    "url": "/static/media/kia3.4191043a.JPG"
+  },
+  {
+    "revision": "5c8687d4da99609df6066bc3e15e24bf",
+    "url": "/static/media/kia5.5c8687d4.JPG"
+  },
+  {
+    "revision": "986013c54e4d84f3a5edd895a01e5b91",
+    "url": "/static/media/mkx2.986013c5.JPG"
+  },
+  {
+    "revision": "9e45202745c76f5e6ad1563b23de67f0",
+    "url": "/static/media/mkx3.9e452027.JPG"
+  },
+  {
+    "revision": "d31e682eb5c84d08d2cb2b8fd21d551b",
+    "url": "/static/media/nissan2.d31e682e.JPEG"
+  },
+  {
+    "revision": "56657f8c4e50751b9f43d78be7896ba1",
+    "url": "/static/media/nissan3.56657f8c.JPEG"
+  },
+  {
+    "revision": "bba89c143f42e07311cbb828b68d4589",
+    "url": "/static/media/quest2.bba89c14.JPG"
+  },
+  {
+    "revision": "b2bc2c8e76202b8ffd79d4e5e306e2e4",
+    "url": "/static/media/quest3.b2bc2c8e.JPG"
+  },
+  {
+    "revision": "604998b365b6bae661aaac68620e421c",
+    "url": "/static/media/rio2.604998b3.JPG"
+  },
+  {
+    "revision": "6586993f1c70b21cd8149ff530997a18",
+    "url": "/static/media/rio3.6586993f.JPG"
+  },
+  {
+    "revision": "18628e714382f2b113e63a02f7f9f4f0",
+    "url": "/static/media/s42.18628e71.JPG"
+  },
+  {
+    "revision": "b8368658c72bb27d6f1571a1f1cdc5a3",
+    "url": "/static/media/s43.b8368658.JPG"
+  },
+  {
+    "revision": "5600cea43512c1f3281c02b3c4be12e8",
+    "url": "/static/media/s44.5600cea4.JPG"
+  },
+  {
+    "revision": "a125b80aa79db43b4dc24bfda0f0c644",
+    "url": "/static/media/sienna2.a125b80a.JPG"
+  },
+  {
+    "revision": "7c973534d8609af102f801e4745d46dd",
+    "url": "/static/media/sienna3.7c973534.JPG"
+  },
+  {
+    "revision": "90ee5ae610574d6ce809b6c1db98e23f",
+    "url": "/static/media/srx2.90ee5ae6.JPEG"
+  },
+  {
+    "revision": "a99bfe9c6725d46ab813be6d8cedcc9f",
+    "url": "/static/media/stang2.a99bfe9c.JPG"
+  },
+  {
+    "revision": "6bfe7ffc6fe94e7149331fd881dfaa50",
+    "url": "/static/media/stang3.6bfe7ffc.JPG"
+  },
+  {
+    "revision": "b186eaa76f1ae9a4d2cdb46e937fec62",
+    "url": "/static/media/stang4.b186eaa7.JPG"
+  },
+  {
+    "revision": "029557f9e30ad012cd51847bfa2f5b3e",
+    "url": "/static/media/stratus.029557f9.JPG"
+  },
+  {
+    "revision": "425503939acfa9616644c53737b41cb9",
+    "url": "/static/media/stratus2.42550393.JPG"
+  },
+  {
+    "revision": "2ee8b7ac268d0562edc84f5eb24bec83",
+    "url": "/static/media/stratus3.2ee8b7ac.JPG"
+  },
+  {
+    "revision": "c5b2ee9dedbaf2c0274ac3a99bb9d7ed",
+    "url": "/static/media/tahoe162.c5b2ee9d.JPG"
+  },
+  {
+    "revision": "0c2796fa69d55e8f85ac685dbdfa0817",
+    "url": "/static/media/tahoe163.0c2796fa.JPG"
+  },
+  {
+    "revision": "f03dba9b172acbe5d06499f2dd085192",
+    "url": "/static/media/tahoe164.f03dba9b.JPG"
+  },
+  {
+    "revision": "9193f5e2780a516f621f999dce97dc85",
+    "url": "/static/media/tahoe2.9193f5e2.JPG"
+  },
+  {
+    "revision": "79229de9ab92874bcaba96efad7dd970",
+    "url": "/static/media/tahoe3.79229de9.JPG"
+  },
+  {
+    "revision": "850454349638ff0a1c62970d6fd7677d",
+    "url": "/static/media/terrain2.85045434.JPG"
+  },
+  {
+    "revision": "535f5e268eec2a06b939ff93ec247b72",
+    "url": "/static/media/trail2.535f5e26.JPG"
+  },
+  {
+    "revision": "c8461c5edf1cedfeb7294a43786aae0b",
+    "url": "/static/media/trail3.c8461c5e.JPG"
+  },
+  {
+    "revision": "e5e900fc145c99ee5810f029b0188e57",
+    "url": "/static/media/versa082.e5e900fc.JPG"
+  },
+  {
+    "revision": "836e2a0526e01af7a9d3140256f8e67e",
+    "url": "/static/media/versa083.836e2a05.JPG"
+  },
+  {
+    "revision": "7cfff5662a49dea482f7f9065b9a524e",
+    "url": "/static/media/versa2.7cfff566.JPG"
+  },
+  {
+    "revision": "074284c84beb012a0dcb0ebf022e5f4d",
+    "url": "/static/media/versa3.074284c8.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5e24e409aa57f96e8bc1aa6e53144bb8.js"
+  "/precache-manifest.797cb4e336165308fda148d09175a3a8.js"
 );
 
 self.addEventListener('message', (event) => {
