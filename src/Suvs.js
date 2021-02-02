dcad0a1bfa6f8e53f8cf27d9da8248cf43cdfa7b
aabarreda <aabarreda@gmail.com>
2021-02-01 20:35:32 -0800

    added acadia

--- src/Suvs.js
+++ src/Suvs.js
@@ -38,10 +38,27 @@ import ford2 from './assets/cars/ford2.jpg';
 import ford3 from './assets/cars/ford3.jpg';
 import ford4 from './assets/cars/ford4.jpg';
 
+import acadia1 from './assets/trucks/acadia1.jpg';
+import acadia2 from './assets/trucks/acadia2.jpg';
+import acadia3 from './assets/trucks/acadia3.jpg';
+import acadia4 from './assets/trucks/acadia4.jpg';
+
 class Suvs extends React.Component {
     render() {
     return(
         <Stack horizontal horizontalAlign='center' wrap gap='15'>
+            <Stack horizontalAlign='center' className='tile' style={{ boxShadow: Depths.depth8 }}>
+                <Stack padding={2}><Text className='tile-text-p'>2015 GMC Acadia - $14,200</Text></Stack>
+                <Stack padding={3}>
+                <Text className='tile-text-d'>92K Miles, All Power, Automatic Transmission, Back Up Camera, Front Wheel Drive, 3rd Row Seating.</Text>
+                </Stack>
+                <Carousel width="400px" emulateTouch showStatus={false}>
+                    <img src={acadia1} />
+                    <img src={acadia2} />
+                    <img src={acadia3} />
+                    <img src={acadia4} />
+                </Carousel>
+            </Stack>
             <Stack horizontalAlign='center' className='tile' style={{ boxShadow: Depths.depth8 }}>
                 <Stack padding={2}><Text className='tile-text-p'>2011 Ford Explorer - $12,500</Text></Stack>
                 <Stack padding={3}>
