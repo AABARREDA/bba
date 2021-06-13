66ed825786d32ec827287cc2d4eb79cbe127cb79
aabarreda <aabarreda@gmail.com>
2021-06-12 18:50:20 -0700

    Updates

---
+++ precache-manifest.3843967701e4d1519ed59d1dbd8155c0.js
@@ -0,0 +1,286 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2c5b8a38e20d6fd0e6648166b068dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1469d0ebff3d1b138dda",
+    "url": "/static/css/2.47ebf210.chunk.css"
+  },
+  {
+    "revision": "cc1b009cffecc1621eae",
+    "url": "/static/css/main.4f466b0f.chunk.css"
+  },
+  {
+    "revision": "1469d0ebff3d1b138dda",
+    "url": "/static/js/2.b9cc22c0.chunk.js"
+  },
+  {
+    "revision": "3486e35225962484dde4c4fb2ca1bd90",
+    "url": "/static/js/2.b9cc22c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc1b009cffecc1621eae",
+    "url": "/static/js/main.df8af243.chunk.js"
+  },
+  {
+    "revision": "dfda05006be39051ceae",
+    "url": "/static/js/runtime-main.1968646e.js"
+  },
+  {
+    "revision": "fda78185df083b42bab59fab0491093f",
+    "url": "/static/media/GTI.fda78185.JPG"
+  },
+  {
+    "revision": "5dbd5f6a8b1bf06fe1c922dda910fdbc",
+    "url": "/static/media/HY1.5dbd5f6a.jpg"
+  },
+  {
+    "revision": "b337da5a1986c5fb69755606bc27e3df",
+    "url": "/static/media/HY2.b337da5a.jpg"
+  },
+  {
+    "revision": "1bf060870f35c40dd67abe72dcd8ef49",
+    "url": "/static/media/HY3.1bf06087.jpg"
+  },
+  {
+    "revision": "185ba7cb70156cab43cc9b1193b0cd91",
+    "url": "/static/media/HY4.185ba7cb.jpg"
+  },
+  {
+    "revision": "9721fa731508c87b0407c468da60ace9",
+    "url": "/static/media/ac1.9721fa73.jpg"
+  },
+  {
+    "revision": "0bf67242b1dfc8d315650b02e9ff6a4f",
+    "url": "/static/media/ac2.0bf67242.jpg"
+  },
+  {
+    "revision": "55b8c63bea97fe10268317ea76d57e98",
+    "url": "/static/media/ac3.55b8c63b.jpg"
+  },
+  {
+    "revision": "8590c9b88592a3192e2b75c1db9a1fd4",
+    "url": "/static/media/ac4.8590c9b8.jpg"
+  },
+  {
+    "revision": "2121d313ee655ef6e149784ca24562e0",
+    "url": "/static/media/acadia1.2121d313.jpg"
+  },
+  {
+    "revision": "36193ac73bdb76e1add45abbce9fbb6a",
+    "url": "/static/media/acadia2.36193ac7.jpg"
+  },
+  {
+    "revision": "d1bb2f5a4f43f6d3eab7232dd06180ee",
+    "url": "/static/media/acadia3.d1bb2f5a.jpg"
+  },
+  {
+    "revision": "82819353679aa5ac787e9c09519671ee",
+    "url": "/static/media/acadia4.82819353.jpg"
+  },
+  {
+    "revision": "f4fa1b802ea6675a21960a771daa37a1",
+    "url": "/static/media/cbr1.f4fa1b80.jpg"
+  },
+  {
+    "revision": "e418adbc6afc3d84e94ecbfc261edf9f",
+    "url": "/static/media/cbr2.e418adbc.jpg"
+  },
+  {
+    "revision": "db8d530784fa8968992566d475a893f3",
+    "url": "/static/media/cbr3.db8d5307.jpg"
+  },
+  {
+    "revision": "f81703c7ad8987ab2a77cb3554d2efaf",
+    "url": "/static/media/cbr4.f81703c7.jpg"
+  },
+  {
+    "revision": "6e52ad75490e6ac9ee18a0bfd8db2fd7",
+    "url": "/static/media/cts1.6e52ad75.jpg"
+  },
+  {
+    "revision": "fc6055a1f28bc85abad8279b999d11f3",
+    "url": "/static/media/cts2.fc6055a1.jpg"
+  },
+  {
+    "revision": "4a30b5cba22d11f375cc77d8a2eeb452",
+    "url": "/static/media/cts3.4a30b5cb.jpg"
+  },
+  {
+    "revision": "b4926c93a3a4310e3644e3a95e0e2537",
+    "url": "/static/media/cts4.b4926c93.jpg"
+  },
+  {
+    "revision": "b7694f8402393e0471a5bdc6a08d3897",
+    "url": "/static/media/dealer2.b7694f84.jpg"
+  },
+  {
+    "revision": "40265989b937421d6f034b147673cf1b",
+    "url": "/static/media/fiesta1.40265989.jpg"
+  },
+  {
+    "revision": "4b1db2d5fbf4d3b7b89ea0562561a60d",
+    "url": "/static/media/fiesta2.4b1db2d5.jpg"
+  },
+  {
+    "revision": "927ac6e097a06e52bba8e14d5621d33b",
+    "url": "/static/media/fiesta3.927ac6e0.jpg"
+  },
+  {
+    "revision": "145ac41e7544456ce7685cd708a21101",
+    "url": "/static/media/fiesta4.145ac41e.jpg"
+  },
+  {
+    "revision": "805fde30b1685197cf91c9bec6eed5e5",
+    "url": "/static/media/focus2.805fde30.JPG"
+  },
+  {
+    "revision": "c0a963babb8db65119cf62cc1c46bb73",
+    "url": "/static/media/focus3.c0a963ba.JPG"
+  },
+  {
+    "revision": "0336ac17436f3cb7ab94066a7889cf89",
+    "url": "/static/media/gti2.0336ac17.JPG"
+  },
+  {
+    "revision": "4867232e5c70eb8ee8419c5fdcd7a615",
+    "url": "/static/media/gti3.4867232e.JPG"
+  },
+  {
+    "revision": "2a91f43ac4e77618a64252644f2dc716",
+    "url": "/static/media/gti4.2a91f43a.JPG"
+  },
+  {
+    "revision": "c735de610cdfca25e4b0845a53f210d0",
+    "url": "/static/media/impala1.c735de61.jpg"
+  },
+  {
+    "revision": "d37e68a59c56653b8e95f60c026e674a",
+    "url": "/static/media/impala2.d37e68a5.jpg"
+  },
+  {
+    "revision": "356ed3495a0f539b7b0428ff614bef6d",
+    "url": "/static/media/impala3.356ed349.jpg"
+  },
+  {
+    "revision": "257e5079197cc00506893de27ce1d708",
+    "url": "/static/media/impala4.257e5079.jpg"
+  },
+  {
+    "revision": "97b02d0d93fad5a9dc9bcd5ed227b843",
+    "url": "/static/media/m1.97b02d0d.jpg"
+  },
+  {
+    "revision": "c231b1ea7eb3a31317413e19490043ed",
+    "url": "/static/media/m2.c231b1ea.jpg"
+  },
+  {
+    "revision": "1c9606aa1256c8108a9694d2dc51f952",
+    "url": "/static/media/m3.1c9606aa.jpg"
+  },
+  {
+    "revision": "a50570108849bf974096398b882761dc",
+    "url": "/static/media/m4.a5057010.jpg"
+  },
+  {
+    "revision": "cb400d3ef60523f0912c0ff9589718ff",
+    "url": "/static/media/rio1.cb400d3e.jpg"
+  },
+  {
+    "revision": "43382f9fa8e731b2521831a45ea284b5",
+    "url": "/static/media/rio2.43382f9f.jpg"
+  },
+  {
+    "revision": "e907835b2b7314e6b8bb586009b25bd2",
+    "url": "/static/media/rio3.e907835b.jpg"
+  },
+  {
+    "revision": "98019381ace1bcbe78ea9f2cf1ccd8ec",
+    "url": "/static/media/rio4.98019381.jpg"
+  },
+  {
+    "revision": "c0405b7e84a9074a79bbdb2634b834dd",
+    "url": "/static/media/savana01.c0405b7e.jpeg"
+  },
+  {
+    "revision": "92f2d9de23c9d27af89878963e1c84d4",
+    "url": "/static/media/savana02.92f2d9de.jpeg"
+  },
+  {
+    "revision": "d5b68a7a6c1c9df2a211debef00ae9a2",
+    "url": "/static/media/sentra12v1.d5b68a7a.jpg"
+  },
+  {
+    "revision": "46db97cde02bf8b3612f1b3ef14934a9",
+    "url": "/static/media/sentra12v2.46db97cd.jpg"
+  },
+  {
+    "revision": "373369d57f5763a3f8ee5c744b72d464",
+    "url": "/static/media/sentra12v3.373369d5.jpg"
+  },
+  {
+    "revision": "574fc17b7ebef6cbf994ba165ac3ec9c",
+    "url": "/static/media/sentra12v4.574fc17b.jpg"
+  },
+  {
+    "revision": "366186975d241ff3a86c15f0f816e5fb",
+    "url": "/static/media/silverado1.36618697.jpg"
+  },
+  {
+    "revision": "9f0d526c5e4eef3482db3b2fb5d806a4",
+    "url": "/static/media/silverado2.9f0d526c.jpg"
+  },
+  {
+    "revision": "3cb25612c0f188bf22d061c97d516000",
+    "url": "/static/media/silverado3.3cb25612.jpg"
+  },
+  {
+    "revision": "aeeabf8fbb0b0a6090175e2b8d8e06c6",
+    "url": "/static/media/silverado4.aeeabf8f.jpg"
+  },
+  {
+    "revision": "d4fbbc5a96fd618e844ff5be130793eb",
+    "url": "/static/media/tc1.d4fbbc5a.jpg"
+  },
+  {
+    "revision": "a6ab2b554976c619b35e1fa678452032",
+    "url": "/static/media/tc2.a6ab2b55.jpg"
+  },
+  {
+    "revision": "483e17a54c60a3ce470fdd4cb34bacd1",
+    "url": "/static/media/tc3.483e17a5.jpg"
+  },
+  {
+    "revision": "7d4e151b0a6f9d02c1c5faffda2cae08",
+    "url": "/static/media/tc4.7d4e151b.jpg"
+  },
+  {
+    "revision": "84db33e798b2899ebaf7ab13a6502a27",
+    "url": "/static/media/terrain1.84db33e7.jpg"
+  },
+  {
+    "revision": "5086cf52990d06ee01bea4025be31b3a",
+    "url": "/static/media/terrain2.5086cf52.jpg"
+  },
+  {
+    "revision": "5d3f4f6a39da883987a60e02fe35d9b1",
+    "url": "/static/media/terrain3.5d3f4f6a.jpg"
+  },
+  {
+    "revision": "b2d8512580714b9b13ff3a1365379abe",
+    "url": "/static/media/terrain4.b2d85125.jpg"
+  },
+  {
+    "revision": "baefd48a0cecfc0a8a5cce0bad7cc62d",
+    "url": "/static/media/versa1.baefd48a.jpg"
+  },
+  {
+    "revision": "ee72d667b714c66ccf19c72386611918",
+    "url": "/static/media/versa2.ee72d667.jpg"
+  },
+  {
+    "revision": "c8747661c670fac9017121302aff59ca",
+    "url": "/static/media/versa3.c8747661.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d32277031aa8044879fc27c4b8078354.js"
+  "/precache-manifest.3843967701e4d1519ed59d1dbd8155c0.js"
 );
 
 self.addEventListener('message', (event) => {
