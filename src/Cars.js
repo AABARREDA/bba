01fc698da3dfc8f25af3e0f270eabdc1b0b19fb9
Alberto Barreda <Alberto Barreda>
2023-07-15 20:06:35 -0700

    updated july

--- src/Cars.js
+++ src/Cars.js
@@ -26,16 +26,52 @@ import fm4 from './assets/cars/fm4.jpg'
 
 import cs1 from './assets/cars/cs1.jpg'
 import cs2 from './assets/cars/cs2.jpg'
+import cs3 from './assets/cars/cs3.jpg'
+import cs4 from './assets/cars/cs4.jpg'
 
 import dd1 from './assets/cars/dd1.jpg'
 import dd2 from './assets/cars/dd2.jpg'
 import dd3 from './assets/cars/dd3.jpg'
 import dd4 from './assets/cars/dd4.jpg'
 
+import vnote1 from './assets/cars/vnote1.jpg'
+import vnote2 from './assets/cars/vnote2.jpg'
+import vnote3 from './assets/cars/vnote3.jpg'
+import vnote4 from './assets/cars/vnote4.jpg'
+
+import impas1 from './assets/cars/impas1.jpg'
+import impas2 from './assets/cars/impas2.jpg'
+import impas3 from './assets/cars/impas3.jpg'
+import impas4 from './assets/cars/impas4.jpg'
+
 class Cars extends React.Component {
     render() {
     return(
         <Stack horizontal horizontalAlign='center' wrap gap='15'>
+            <Stack horizontalAlign='center' className='tile' style={{ boxShadow: Depths.depth8 }}>
+                <Stack padding={2}><Text className='tile-text-p'>2012 Chevy Impala - $5,200</Text></Stack>
+                <Stack padding={3}>
+                <Text className='tile-text-d'>165K Miles, All Power, Auto, Front Wheel Drive, Dependable and Clean.</Text>
+                </Stack>
+                <Carousel width="450px" emulateTouch showStatus={false}>
+                    <img src={impas1} />
+                    <img src={impas2} />
+                    <img src={impas3} />
+                    <img src={impas4} />
+                </Carousel>
+            </Stack>
+            <Stack horizontalAlign='center' className='tile' style={{ boxShadow: Depths.depth8 }}>
+                <Stack padding={2}><Text className='tile-text-p'>2015 Nissa Versa Note - $7,500</Text></Stack>
+                <Stack padding={3}>
+                <Text className='tile-text-d'>108K Miles, All Power, Automatic, Front Wheel Drive, AC, Extra Clean.</Text>
+                </Stack>
+                <Carousel width="450px" emulateTouch showStatus={false}>
+                    <img src={vnote1} />
+                    <img src={vnote2} />
+                    <img src={vnote3} />
+                    <img src={vnote4} />
+                </Carousel>
+            </Stack>
              <Stack horizontalAlign='center' className='tile' style={{ boxShadow: Depths.depth8 }}>
                 <Stack padding={2}><Text className='tile-text-p'>2013 Dodge Dart - $7,500</Text></Stack>
                 <Stack padding={3}>
@@ -54,8 +90,10 @@ class Cars extends React.Component {
                 <Text className='tile-text-d'>78K Miles, Leather, All Power, 6 Cyl. Auto, Front Wheel Drive .</Text>
                 </Stack>
                 <Carousel width="450px" emulateTouch showStatus={false}>
+                    <img src={cs3} />
                     <img src={cs1} />
                     <img src={cs2} />
+                    <img src={cs4} />
                 </Carousel>
             </Stack>
             <Stack horizontalAlign='center' className='tile' style={{ boxShadow: Depths.depth8 }}>
--- src/Suvs.js
+++ src/Suvs.js
@@ -32,10 +32,78 @@ import fe4 from './assets/trucks/fe4.jpg';
 import yd1 from './assets/trucks/yd1.jpg';
 import yd2 from './assets/trucks/yd2.jpg';
 
+import equi1 from './assets/trucks/equi1.jpg';
+import equi2 from './assets/trucks/equi2.jpg';
+import equi3 from './assets/trucks/equi3.jpg';
+import equi4 from './assets/trucks/equi4.jpg';
+
+import te1 from './assets/trucks/te1.jpg';
+import te2 from './assets/trucks/te2.jpg';
+import te3 from './assets/trucks/te3.jpg';
+import te4 from './assets/trucks/te4.jpg';
+
+import edge1 from './assets/trucks/edge1.jpg';
+import edge2 from './assets/trucks/edge2.jpg';
+import edge3 from './assets/trucks/edge3.jpg';
+import edge4 from './assets/trucks/edge4.jpg';
+
+import rado1 from './assets/trucks/rado1.jpg';
+import rado2 from './assets/trucks/rado2.jpg';
+import rado3 from './assets/trucks/rado3.jpg';
+import rado4 from './assets/trucks/rado4.jpg';
+
 class Suvs extends React.Component {
     render() {
     return(
         <Stack horizontal horizontalAlign='center' wrap gap='15'>
+            <Stack horizontalAlign='center' className='tile' style={{ boxShadow: Depths.depth8 }}>
+                <Stack padding={2}><Text className='tile-text-p'>2012 Chevy Silverado 2500 - $21,000</Text></Stack>
+                <Stack padding={3}>
+                <Text className='tile-text-d'>62K miles, Oil Change, AC, New wheels and tires, great truck.</Text>
+                </Stack>
+                <Carousel width="450px" emulateTouch showStatus={false}>
+                    <img src={rado1} />
+                    <img src={rado2} />
+                    <img src={rado3} />
+                    <img src={rado4} />
+                </Carousel>
+            </Stack>
+            <Stack horizontalAlign='center' className='tile' style={{ boxShadow: Depths.depth8 }}>
+                <Stack padding={2}><Text className='tile-text-p'>2010 Ford Edge - $7,700</Text></Stack>
+                <Stack padding={3}>
+                <Text className='tile-text-d'>130K Miles, Leather, v-6, Auto, Front Wheel Drive.</Text>
+                </Stack>
+                <Carousel width="450px" emulateTouch showStatus={false}>
+                    <img src={edge1} />
+                    <img src={edge2} />
+                    <img src={edge3} />
+                    <img src={edge4} />
+                </Carousel>
+            </Stack>
+            <Stack horizontalAlign='center' className='tile' style={{ boxShadow: Depths.depth8 }}>
+                <Stack padding={2}><Text className='tile-text-p'>2012 GMC Terrain - $11,500</Text></Stack>
+                <Stack padding={3}>
+                <Text className='tile-text-d'>108K Miles, All Wheel Drive, Leather, Sunroof, Back Up Camera.</Text>
+                </Stack>
+                <Carousel width="450px" emulateTouch showStatus={false}>
+                    <img src={te1} />
+                    <img src={te2} />
+                    <img src={te3} />
+                    <img src={te4} />
+                </Carousel>
+            </Stack>
+            <Stack horizontalAlign='center' className='tile' style={{ boxShadow: Depths.depth8 }}>
+                <Stack padding={2}><Text className='tile-text-p'>2015 Chevy Equinox - $11,500</Text></Stack>
+                <Stack padding={3}>
+                <Text className='tile-text-d'>125K Miles, All Wheel Drive, Leather, AC, Nice/Clean.</Text>
+                </Stack>
+                <Carousel width="450px" emulateTouch showStatus={false}>
+                    <img src={equi1} />
+                    <img src={equi2} />
+                    <img src={equi3} />
+                    <img src={equi4} />
+                </Carousel>
+            </Stack>
             <Stack horizontalAlign='center' className='tile' style={{ boxShadow: Depths.depth8 }}>
                 <Stack padding={2}><Text className='tile-text-p'>2015 GMC Yukon - $22,500</Text></Stack>
                 <Stack padding={3}>
